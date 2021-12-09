9eae14b46997c79ed8df0b15026279115fc20974
Lorenz Beyer <lorenzbeyer@googlemail.com>
2021-12-09 17:33:20 +0100

    build and deploy

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ng build --base-href=boardgame-collection
-git add -A
-git commit -m "build and deploy"
-git push
+ng build --base-href=boardgame-collection &&
+git add -A &&
+git commit -m "build and deploy" &&
+git push &&
 git subtree push --prefix dist origin gh-pages
